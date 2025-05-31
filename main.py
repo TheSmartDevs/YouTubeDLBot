a0e6c96faa94738d270ba71993289d2f9899b2f8
Abir Arafat Chawdhury <abirxdhackz.info.me@gmail.com>
2025-05-31 19:38:26 +0600

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ from utils import LOGGER
 from app import app  
 from dlxutils import setup_dl_handler
 from sudo import setup_sudo_handlers
+from core import setup_start_handler
 
 #Connect Modules
 setup_dl_handler(app)
 setup_sudo_handlers(app)
+setup_start_handler(app)
 
 LOGGER.info("Bot Successfully Started! 💥")
 app.run()

d968342d54d8ba7cb9f214767351da628daa27e7
Abir Arafat Chawdhury <abirxdhackz.info.me@gmail.com>
2026-02-20 21:13:37 +0600

    Create config.py

---
+++ config.py
@@ -0,0 +1,26 @@
+#Copyright @ISmartCoder
+#Updates Channel @abirxdhackz
+API_ID = YOUR_API_ID
+API_HASH = 'YOUR_API_HASH'
+BOT_TOKEN = 'YOUR_BOT_TOKEN'
+UPDATE_CHANNEL_URL = 't.me/abirxdhackz'
+COMMAND_PREFIXES = ['/', '!', '.', ',', '$', '#']
+
+OWNER_ID = 123456789
+DEVELOPER_USER_ID = 123456789
+LOG_CHANNEL_ID = -1001234567890
+
+VIDEO_QUALITY_OPTIONS = {
+    "1080p": {"label": "1080p Full HD", "height": 1080},
+    "720p":  {"label": "720p HD",       "height": 720},
+    "480p":  {"label": "480p SD",       "height": 480},
+    "360p":  {"label": "360p Low",      "height": 360},
+    "144p":  {"label": "144p Very Low", "height": 144},
+}
+
+AUDIO_QUALITY_OPTIONS = {
+    "320kbps": {"label": "320kbps Best",   "bitrate": "320"},
+    "256kbps": {"label": "256kbps High",   "bitrate": "256"},
+    "128kbps": {"label": "128kbps Medium", "bitrate": "128"},
+    "64kbps":  {"label": "64kbps Low",     "bitrate": "64"},
+}
